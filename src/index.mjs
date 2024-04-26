6e42b556b7d2b7585483005ae6202d2daacd86d7
Alfred Crosby <alfred@vincere.dev>
2024-04-26 20:06:11 +0700

    Update package version and code to 1.0.2

--- src/index.mjs
+++ src/index.mjs
@@ -14,14 +14,17 @@ function getDiff() {
 }
 
 const prompt = (diff) => `
-Generate a git commit message for the following code diff with the following specification:
-1. The message must be a maximum 80 characters.
-2. Summarize the changes, what is the file changed, what the new change does, in one sentence.
-3. Language must be English and in the form of present tense.
-4. IMPORTANT! Please respond with ONLY the commit message. EXCLUDE everything else! Your message will be used as a commit message directly.
+Generate a git commit message for the following code diff with the following IMPORTANT specification:
+1. The message must be a MAXIMUM 80 CHARACTERS.
+2. Summarize the changes, what is the file changed, what the new change does, is there lines of code removed, in one sentence.
+3. Language must be English and in the form of present tense, and please don't put period after sentence.
+4. Your message will be used as a commit message directly. So, make sure it is clear and concise.
 
 Code diff:
   ${diff}
+
+IMPORTANT:
+PLEASE ONLY WRITE THE COMMIT MESSAGE, DON'T ADD "Here is a ..." or similar phrase, or ADDITIONAL "Note:". ONLY EXACTLY THE MESSAGE.
 `;
 
 async function createCommitMessageFrom(diff) {
